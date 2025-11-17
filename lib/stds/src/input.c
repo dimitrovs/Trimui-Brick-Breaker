85d3f1ace9ad1e4dcf0a7a74e75f05e2fda80d9f
Stefan Dimitrov <stefan@dimitrov.li>
2025-11-16 20:18:00 -0500

    Remove excessive debug logging from input.c

--- lib/stds/src/input.c
+++ lib/stds/src/input.c
@@ -30,35 +30,23 @@ process_input( void ) {
     init_controller();
   }
 
-  int event_count = 0;
   while ( SDL_PollEvent( &event ) ) {
-    event_count++;
     SDL_GetMouseState( &app.mouse.x, &app.mouse.y );
-    
-    // Log all events for debugging
-    if ( event.type != SDL_MOUSEMOTION ) {  // Don't spam with mouse motion
-      printf("DEBUG: SDL Event type: %d\n", event.type);
-    }
     
     switch ( event.type ) {
     case SDL_QUIT:
-      printf("DEBUG: SDL_QUIT event\n");
       exit( EXIT_SUCCESS );
       break;
     case SDL_KEYDOWN:
-      printf("DEBUG: SDL_KEYDOWN event - scancode: %d\n", event.key.keysym.scancode);
       key_pressed( &event.key );
       break;
     case SDL_KEYUP:
-      printf("DEBUG: SDL_KEYUP event - scancode: %d\n", event.key.keysym.scancode);
       key_released( &event.key );
       break;
     case SDL_MOUSEBUTTONDOWN:
-      printf("DEBUG: SDL_MOUSEBUTTONDOWN at %d,%d button: %d\n", app.mouse.x, app.mouse.y, event.button.button);
       mouse_pressed( &event.button );
       break;
     case SDL_MOUSEBUTTONUP:
-      printf("DEBUG: SDL_MOUSEBUTTONUP at %d,%d button: %d\n", app.mouse.x, app.mouse.y, event.button.button);
       mouse_released( &event.button );
       break;
     case SDL_MOUSEWHEEL:
@@ -73,10 +61,6 @@ process_input( void ) {
     default:
       break;
     }
-  }
-  
-  if ( event_count > 0 ) {
-    printf("DEBUG: Processed %d events this frame\n", event_count);
   }
 }
 
@@ -121,18 +105,13 @@ mouse_released( SDL_MouseButtonEvent *event ) {
  */
 static void
 init_controller( void ) {
-  printf("DEBUG: Checking for game controllers...\n");
   for ( int i = 0; i < SDL_NumJoysticks(); i++ ) {
     if ( SDL_IsGameController( i ) ) {
       controller = SDL_GameControllerOpen( i );
       if ( controller ) {
-        printf("DEBUG: Controller found and opened: %s\n", SDL_GameControllerName( controller ));
         break;
       }
     }
-  }
-  if ( !controller ) {
-    printf("DEBUG: No game controller found\n");
   }
 }
 
@@ -141,63 +120,49 @@ init_controller( void ) {
  */
 static void
 controller_button_pressed( SDL_ControllerButtonEvent *event ) {
-  printf("DEBUG: Controller button pressed: %d\n", event->button);
-  
   switch ( event->button ) {
     case SDL_CONTROLLER_BUTTON_A:
     case SDL_CONTROLLER_BUTTON_B:
       // A/B button = Space (start game) or action button
       app.keyboard[SDL_SCANCODE_SPACE] = 1;
       button_a_pressed = true;
-      printf("DEBUG: A/B button pressed - action triggered (selected button: %d)\n", selected_button_index);
       break;
     case SDL_CONTROLLER_BUTTON_X:
     case SDL_CONTROLLER_BUTTON_Y:
       // X/Y button = Pause
       app.keyboard[SDL_SCANCODE_P] = 1;
-      printf("DEBUG: X/Y button pressed - pause\n");
       break;
     case SDL_CONTROLLER_BUTTON_DPAD_UP:
       // Move selection up in menu
       selected_button_index--;
       if ( selected_button_index < 0 ) selected_button_index = MAX_MENU_BUTTONS - 1;
-      printf("DEBUG: DPAD UP - button index now: %d\n", selected_button_index);
       break;
     case SDL_CONTROLLER_BUTTON_DPAD_DOWN:
       // Move selection down in menu
       selected_button_index++;
       if ( selected_button_index >= MAX_MENU_BUTTONS ) selected_button_index = 0;
-      printf("DEBUG: DPAD DOWN - button index now: %d\n", selected_button_index);
       break;
     case SDL_CONTROLLER_BUTTON_DPAD_LEFT:
       app.keyboard[SDL_SCANCODE_LEFT] = 1;
-      printf("DEBUG: DPAD LEFT pressed\n");
       break;
     case SDL_CONTROLLER_BUTTON_DPAD_RIGHT:
       app.keyboard[SDL_SCANCODE_RIGHT] = 1;
-      printf("DEBUG: DPAD RIGHT pressed\n");
       break;
     case SDL_CONTROLLER_BUTTON_START:
       // Start = Pause
       app.keyboard[SDL_SCANCODE_P] = 1;
-      printf("DEBUG: START pressed - pause\n");
       break;
     case SDL_CONTROLLER_BUTTON_BACK:
       // Back/Select = Escape (return to menu)
       app.keyboard[SDL_SCANCODE_ESCAPE] = 1;
-      printf("DEBUG: SELECT/BACK pressed - return to menu\n");
       break;
     case SDL_CONTROLLER_BUTTON_GUIDE:
-      printf("DEBUG: MENU pressed\n");
       break;
     case SDL_CONTROLLER_BUTTON_LEFTSHOULDER:
-      printf("DEBUG: L1 pressed\n");
       break;
     case SDL_CONTROLLER_BUTTON_RIGHTSHOULDER:
-      printf("DEBUG: R1 pressed\n");
       break;
     default:
-      printf("DEBUG: Unknown button %d pressed\n", event->button);
       break;
   }
 }
@@ -207,14 +172,11 @@ controller_button_pressed( SDL_ControllerButtonEvent *event ) {
  */
 static void
 controller_button_released( SDL_ControllerButtonEvent *event ) {
-  printf("DEBUG: Controller button released: %d\n", event->button);
-  
   switch ( event->button ) {
     case SDL_CONTROLLER_BUTTON_A:
     case SDL_CONTROLLER_BUTTON_B:
       app.keyboard[SDL_SCANCODE_SPACE] = 0;
       button_a_pressed = false;
-      printf("DEBUG: A/B button released\n");
       break;
     case SDL_CONTROLLER_BUTTON_X:
     case SDL_CONTROLLER_BUTTON_Y:
@@ -222,11 +184,9 @@ controller_button_released( SDL_ControllerButtonEvent *event ) {
       break;
     case SDL_CONTROLLER_BUTTON_DPAD_LEFT:
       app.keyboard[SDL_SCANCODE_LEFT] = 0;
-      printf("DEBUG: DPAD LEFT released\n");
       break;
     case SDL_CONTROLLER_BUTTON_DPAD_RIGHT:
       app.keyboard[SDL_SCANCODE_RIGHT] = 0;
-      printf("DEBUG: DPAD RIGHT released\n");
       break;
     case SDL_CONTROLLER_BUTTON_START:
       app.keyboard[SDL_SCANCODE_P] = 0;
